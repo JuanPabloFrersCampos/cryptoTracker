65b6df461c123d185e82d2ff3cf54575756a9b53
JuanPabloFrersCampos <juanpfrers@gmail.com>
2024-12-07 12:17:45 -0300

    Create Portfolio model: Has a FK to user_id Has a type INT field, which will be filled with an ENUM (1=cryptos, 2=stocks)
    Alter Portfolio model:
    Add FK to Portfolio (all operations belong to one portfolio)
    
    Register Portfolio in Django's admin interface

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Crypto, Operation
+from .models import Crypto, Operation, Portfolio
 
 # Register your models here.
 
 admin.site.register(Crypto)
 admin.site.register(Operation)
+admin.site.register(Portfolio)
--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Portfolio(models.Model):
+    FK_users_id = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    type = models.IntegerField()
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Crypto(models.Model):
     symbol = models.CharField(max_length=4)
     def __str__(self):
@@ -8,6 +18,10 @@ class Crypto(models.Model):
 
 class Operation(models.Model):
     symbol = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+    FK_portfolio_id = models.ForeignKey(
+        Portfolio,
+        on_delete=models.CASCADE
+    )
     cryptoQuantity = models.FloatField()
     price = models.FloatField()
     isSell = models.BooleanField()
