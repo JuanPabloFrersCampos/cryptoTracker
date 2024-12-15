ce8198ec1077d7f88748819e751e2f36994de28f
JuanPabloFrersCampos <juanpfrers@gmail.com>
2024-12-14 22:21:42 -0300

    Clean up settings

--- cryptotracker/settings.py
+++ cryptotracker/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from environ import Env
 
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,12 +60,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache'
-    }
-}
-
 ROOT_URLCONF = 'cryptotracker.urls'
 
 TEMPLATES = [
@@ -110,7 +101,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(env('DATABASE_URL'))
     }
-    CELERY_BROKER_URL = env('REDIS_URL', default='redis://')
+    CELERY_BROKER_URL = env('REDIS_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
